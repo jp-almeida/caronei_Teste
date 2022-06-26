3a6d1eeb556da5088c3769c5b711dfb5873421fd
Joao Pedro <joao.pedro.jpda@gmail.com>
2022-06-26 00:39:17 -0300

    Pular cadastro para homescreen

--- caronei-app/screens/SignUpScreen.js
+++ caronei-app/screens/SignUpScreen.js
@@ -1,73 +1,79 @@
-import { Text, View, TouchableWithoutFeedback, TouchableOpacity, TextInput, SafeAreaView } from "react-native"
+import {
+  Text,
+  View,
+  TouchableWithoutFeedback,
+  TouchableOpacity,
+  TextInput,
+  SafeAreaView,
+} from "react-native"
 import React, { useState } from "react"
 import tw from "twrnc"
-import config from '../config/config.json'
+import config from "../config/config.json"
+import { useNavigation } from "@react-navigation/native"
 // import port from "../Controller.js"
-
 
 //codigo adaptado de https://webdesignemfoco.com/cursos/react-js/integracoes-com-react-native-3-frontend
 
-
 const SignUpScreen = () => {
-    
-    const [email, setEmail] = useState(null)
-    const [password, setPassword] = useState(null)
-    const [message, setMessage] = useState(null) //mensagem de resposta do back-end (se o cadastro foi realizado com sucesso ou nao)
+  const navigation = useNavigation()
+  const [email, setEmail] = useState(null)
+  const [password, setPassword] = useState(null)
+  const [message, setMessage] = useState(null) //mensagem de resposta do back-end (se o cadastro foi realizado com sucesso ou nao)
 
-    async function registerUser(){
-        //gambiarra porque as portas não estavam batendo
-        original_port = config.urlRootNode.split(":")[2]
-        url = config.urlRootNode.replace(original_port, config.backend_port)
-        // console.log(original_port, url)
+  async function registerUser() {
+    //gambiarra porque as portas não estavam batendo
+    original_port = config.urlRootNode.split(":")[2]
+    url = config.urlRootNode.replace(original_port, config.backend_port)
+    // console.log(original_port, url)
 
-        let reqs = await fetch(url +'/create',{
-            method: 'POST',
-            headers:{
-                'Accept':'application/json',
-                'Content-Type':'application/json'
-            },
-            body: JSON.stringify({
-                emailUser: email,
-                passwordUser: password,
-            })
-        });
-        let resp = await reqs.json();
-        setMessage(resp);
-    }
+    let reqs = await fetch(url + "/create", {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        emailUser: email,
+        passwordUser: password,
+      }),
+    })
+    let resp = await reqs.json()
+    setMessage(resp)
+  }
 
-    return (
-        <SafeAreaView style={tw`bg-white h-full`}>
-            <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
-                <View  style={tw`p-10 pt-50`}>
+  return (
+    <SafeAreaView style={tw`bg-white h-full`}>
+      <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
+        <View style={tw`p-10 pt-50`}>
+          <View style={{}}>
+            {message && <Text>{message}</Text>}
+            <TextInput
+              style={{}}
+              placeholder="Digite seu email"
+              onChangeText={(text) => setEmail(text)}
+            />
 
-                    <View style={{}}>
-                        {message && (
-                            <Text>{message}</Text>)}
-                        <TextInput
-                            style={{}}
-                            placeholder="Digite seu email"
-                            onChangeText={(text) => setEmail(text)}
-                        />
+            <TextInput
+              style={{}}
+              placeholder="Digite a senha:"
+              secureTextEntry={true}
+              onChangeText={(text) => setPassword(text)}
+            />
 
-
-                        <TextInput
-                            style={{}}
-                            placeholder="Digite a senha:"
-                            secureTextEntry={true}
-                            onChangeText={(text) => setPassword(text)}
-                        />
-
-                        <TouchableOpacity style={{}} onPress={registerUser}>
-                            <Text style={{}}>Enviar</Text>
-                        </TouchableOpacity>
-                        <TouchableOpacity style={{}} onPress={console.log("IR PARA A HOME SCREEN")}>
-                            <Text style={{}}>Pular cadastro</Text>
-                        </TouchableOpacity>
-                    </View>
-                </View>
-            </TouchableWithoutFeedback>
-        </SafeAreaView>
-    )
+            <TouchableOpacity style={{}} onPress={registerUser}>
+              <Text style={{}}>Enviar</Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              style={{}}
+              onPress={() => navigation.navigate("HomeScreen")}
+            >
+              <Text style={{}}>Pular cadastro</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </TouchableWithoutFeedback>
+    </SafeAreaView>
+  )
 }
 
 export default SignUpScreen
