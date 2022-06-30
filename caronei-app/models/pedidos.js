14e72a0af32e2317844ea0d38e11e94cf0db587e
caiofov <cfoviana@alu.ufc.br>
2022-06-30 19:01:35 -0300

    tabelas de rotas e match

---
+++ caronei-app/models/pedidos.js
@@ -0,0 +1,35 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Pedidos extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Pedidos.init({
+    matriculaPedido: {
+      type: DataTypes.INTEGER,
+      references:{
+        model:"usuarios",
+        key:"matricula"
+      }
+    },
+    nomeDestino: DataTypes.STRING,
+    nomePartida: DataTypes.STRING,
+    latitudeDestino: DataTypes.FLOAT,
+    longitudeDestino: DataTypes.FLOAT,
+    latitudePartida: DataTypes.FLOAT,
+    latitudeDestino: DataTypes.FLOAT
+  }, {
+    sequelize,
+    modelName: 'Pedidos',
+  });
+  return Pedidos;
+};
