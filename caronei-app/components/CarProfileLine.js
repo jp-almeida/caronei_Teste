838be475124864096f768741389b4f301790559c
caiofov <cfoviana@alu.ufc.br>
2022-07-04 14:53:02 -0300

    styling

--- caronei-app/components/CarProfileLine.js
+++ caronei-app/components/CarProfileLine.js
@@ -6,7 +6,7 @@ import { store } from '../store'
 import config from '../config/config.json'
 import { updateCar, deleteCar } from '../requestsFunctions'
 import DeleteButton from './buttons/DeleteButton'
-
+import { styles } from '../styles'
 
 
 const CarProfileLine = (props) => {
@@ -18,38 +18,43 @@ const CarProfileLine = (props) => {
     return (
         <View>
             <Text>{props.carro.placa} {props.carro.modelo} {props.carro.cor}</Text>
-        
-            <EditButton element = {props.carro} editFunction = {props.editFunction}/>
-            <DeleteButton deleteFunction = {()=>{
+
+            <EditButton element={props.carro} editFunction={props.editFunction} />
+            <DeleteButton deleteFunction={() => {
                 deleteCar(props.carro.placa)
-                props.deleteFunction()}
-                }/>
-            <Dialog 
+                props.deleteFunction()
+            }
+            } />
+            <Dialog
                 visible={props.carro.isEditing}
                 onTouchOutside={props.editFunction}
+                overlayStyle={styles.dialog}
             >
-                <Dialog.Title title="Editar"/>
-                    <Text>Placa</Text>
-                    <TextInput
-                        style={{}}
-                        defaultValue={props.carro.placa}
-                        onChangeText={(text) => {placa = text}}
-                    />
-                    <Text>Modelo</Text>
-                    <TextInput
-                        style={{}}
-                        defaultValue={props.carro.modelo}
-                        onChangeText={(text) => {modelo = text}}
-                    />
-                    <Text>Cor</Text>
-                    <TextInput
-                        style={{}}
-                        defaultValue={props.carro.cor}
-                        onChangeText={(text) => {cor = text}}
-                    />
-                    <Dialog.Button title="Adicionar" onPress = {() => {
-                        let resp = updateCar(placa,props.carro.placa, modelo,cor)
-                        if(resp){
+                <Dialog.Title title="Editar carro" titleStyle={styles.dialogTitle} />
+                <Text>Placa</Text>
+                <TextInput
+                    style={styles.textInput}
+                    defaultValue={props.carro.placa}
+                    onChangeText={(text) => { placa = text }}
+                />
+                <Text>Modelo</Text>
+                <TextInput
+                    style={styles.textInput}
+                    defaultValue={props.carro.modelo}
+                    onChangeText={(text) => { modelo = text }}
+                />
+                <Text>Cor</Text>
+                <TextInput
+                    style={styles.textInput}
+                    defaultValue={props.carro.cor}
+                    onChangeText={(text) => { cor = text }}
+                />
+                <Dialog.Button
+                    title="Adicionar"
+                    style={styles.button}
+                    onPress={() => {
+                        let resp = updateCar(placa, props.carro.placa, modelo, cor)
+                        if (resp) {
                             props.editFunction({
                                 ...props.carro,
                                 placa: placa,
@@ -59,14 +64,14 @@ const CarProfileLine = (props) => {
                                 changed: true
                             })
                         }
-                        
-                    }}/>
-                    <Dialog.Button title="Cancelar" onPress = {() => 
-                        props.editFunction({
-                            ...props.carro,
-                            isEditing: false
-                        })}/>
-                </Dialog>
+
+                    }} />
+                <Dialog.Button title="Cancelar" onPress={() =>
+                    props.editFunction({
+                        ...props.carro,
+                        isEditing: false
+                    })} />
+            </Dialog>
         </View>
     )
 }
