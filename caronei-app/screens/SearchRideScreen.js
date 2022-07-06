904f6afa45da2326612a5a7874361ef5c1777f14
Joao Pedro <joao.pedro.jpda@gmail.com>
2022-07-05 21:02:16 -0300

    removendo } da linha 44

--- caronei-app/screens/SearchRideScreen.js
+++ caronei-app/screens/SearchRideScreen.js
@@ -18,134 +18,155 @@ import { searchPassageiro, searchDriver } from "../requestsFunctions"
 import { useDispatch } from "react-redux"
 
 const SearchRideScreen = ({ route }) => {
-    const dispatch = useDispatch()
-    const navigation = useNavigation()
-    const [name, setName] = useState(null)
-    const [partida, setPartida] = useState(null)
-    const [destino, setDestino] = useState(null)
-    const [match, setMatch] = useState(null)
-    const { parametro } = route.params
-    var avaliacao = 5
-    let procurando = false
+  const dispatch = useDispatch()
+  const navigation = useNavigation()
+  const [name, setName] = useState(null)
+  const [partida, setPartida] = useState(null)
+  const [destino, setDestino] = useState(null)
+  const [match, setMatch] = useState(null)
+  const { parametro } = route.params
+  var avaliacao = 5
+  let procurando = false
 
+  async function procurarPassageiro() {
+    const response = await searchPassageiro(parametro)
 
-    async function procurarPassageiro() {
-        const response = await searchPassageiro(parametro)
-
-        if (await response.response) {
-            setMatch(await response.pedidos)
-            navigation.navigate("AcceptRideScreen", { corrida: await response.pedidos })
-        }
-        else {
-            procurando = false
-            console.log("Não achou")
-        }
+    if (await response.response) {
+      setMatch(await response.pedidos)
+      navigation.navigate("AcceptRideScreen", {
+        corrida: await response.pedidos,
+      })
+    } else {
+      procurando = false
+      console.log("Não achou")
     }
   }
 
-    async function procurarMotorista() {
-        const response = await searchDriver(parametro)
-        if (typeof (response) != "string") {
-            // FAZER O MATCH!!!!!!
-            console.log("DEU MATCH!!!!!!!!!!")
-        }
-        else {
-            console.log("Nenhum motorista por enquanto")
-        }
+  async function procurarMotorista() {
+    const response = await searchDriver(parametro)
+    if (typeof response != "string") {
+      // FAZER O MATCH!!!!!!
+      console.log("DEU MATCH!!!!!!!!!!")
+    } else {
+      console.log("Nenhum motorista por enquanto")
     }
+  }
 
-    if (store.getState().ride.role == MOTORISTA) {
-        procurarPassageiro()
-    }
-    else {
-        console.log(parametro) //printa a rota
-    }
+  if (store.getState().ride.role == MOTORISTA) {
+    procurarPassageiro()
+  } else {
+    console.log(parametro) //printa a rota
+  }
 
+  return (
+    <SafeAreaView style={tw`bg-white h-full`}>
+      <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
+        <View style={{ backgroundColor: "#EFE9E5", flex: 1 }}>
+          <View
+            style={{
+              width: "100%",
+              height: undefined,
+              aspectRatio: 1,
+            }}
+          >
+            <Map />
+          </View>
+          <View
+            style={{
+              //'rgba(144,144, 144, 0.1)'
+              justifyContent: "center",
+              alignItems: "center",
+              margin: 10,
+            }}
+          >
+            <Text></Text>
 
-    return (
-        <SafeAreaView style={tw`bg-white h-full`}>
-            <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
-                <View style={{ backgroundColor: '#EFE9E5', flex: 1 }}>
-                    <View style={{
-                        width: '100%',
-                        height: undefined,
-                        aspectRatio: 1,
-                    }}>
-                        <Map />
-                    </View>
-                    <View style={{
-                        //'rgba(144,144, 144, 0.1)'
-                        justifyContent: "center",
-                        alignItems: "center",
-                        margin: 10}}>
-                        <Text ></Text>
-
-                        <View style={{
-                            flexDirection: 'row',
-                            justifyContent: 'space-around'}}>
-                            <View>
-                                <Icon name="account-circle" type="material" size={100} />
-                            </View>
-                            <View>
-                                <View style={{
-                                    backgroundColor: '#4D4C7D',
-                                    borderRadius: 25}}>
-                                    {store.getState().ride.role == MOTORISTA ?
-                                        <Text style={{ color: 'white' }}>    Procurando passageiro... </Text>
-                                        :
-                                        <Text style={{ color: 'white' }}>    Procurando motorista... </Text>
-                                    }
-
-
-                                </View>
-
-                                <View style={{
-                                    flexDirection: 'row',
-                                    padding: 10,}}>
-                                    <View style={{ backgroundColor: '#4D4C7D', borderRadius: 25, }}>
-                                        <Text style={{ color: 'white' }}> <Icon name="stars" type="material" size={15} />   ?   </Text>
-                                    </View>
-
-                                </View>
-
-                            </View>
-                        </View>
-
-
-                    </View>
-                    <View style={{
-                        marginTop: 100,
-                        padding: 5,
-
-                        flexDirection: 'row',
-                        justifyContent: 'space-around',
-                    }}>
-                        <DefaultButton
-                            title="Atualizar Busca"
-                            onPress={() => {
-                                if (store.getState().ride.role == PASSAGEIRO) {
-                                    procurarMotorista()
-                                }
-                                // navigation.navigate('HomeScreen')
-                            }} />
-                    </View>
-                    <View style={{
-                        padding: 5,
-
-                        flexDirection: 'row',
-                        justifyContent: 'space-around',
-                    }}>
-                        <DefaultButton
-                            title="Cancelar viagem"
-                            onPress={() => {
-                                dispatch(cancelar_corrida())
-                                navigation.navigate('HomeScreen')
-                            }} />
-                    </View>
+            <View
+              style={{
+                flexDirection: "row",
+                justifyContent: "space-around",
+              }}
+            >
+              <View>
+                <Icon name="account-circle" type="material" size={100} />
+              </View>
+              <View>
+                <View
+                  style={{
+                    backgroundColor: "#4D4C7D",
+                    borderRadius: 25,
+                  }}
+                >
+                  {store.getState().ride.role == MOTORISTA ? (
+                    <Text style={{ color: "white" }}>
+                      {" "}
+                      Procurando passageiro...{" "}
+                    </Text>
+                  ) : (
+                    <Text style={{ color: "white" }}>
+                      {" "}
+                      Procurando motorista...{" "}
+                    </Text>
+                  )}
                 </View>
-            </TouchableWithoutFeedback >
-        </SafeAreaView >
-    )
+
+                <View
+                  style={{
+                    flexDirection: "row",
+                    padding: 10,
+                  }}
+                >
+                  <View
+                    style={{ backgroundColor: "#4D4C7D", borderRadius: 25 }}
+                  >
+                    <Text style={{ color: "white" }}>
+                      {" "}
+                      <Icon name="stars" type="material" size={15} /> ?{" "}
+                    </Text>
+                  </View>
+                </View>
+              </View>
+            </View>
+          </View>
+          <View
+            style={{
+              marginTop: 100,
+              padding: 5,
+
+              flexDirection: "row",
+              justifyContent: "space-around",
+            }}
+          >
+            <DefaultButton
+              title="Atualizar Busca"
+              onPress={() => {
+                if (store.getState().ride.role == PASSAGEIRO) {
+                  procurarMotorista()
+                }
+                // navigation.navigate('HomeScreen')
+              }}
+            />
+          </View>
+          <View
+            style={{
+              padding: 5,
+
+              flexDirection: "row",
+              justifyContent: "space-around",
+            }}
+          >
+            <DefaultButton
+              title="Cancelar viagem"
+              onPress={() => {
+                dispatch(cancelar_corrida())
+                navigation.navigate("HomeScreen")
+              }}
+            />
+          </View>
+        </View>
+      </TouchableWithoutFeedback>
+    </SafeAreaView>
+  )
 }
 
 export default SearchRideScreen
